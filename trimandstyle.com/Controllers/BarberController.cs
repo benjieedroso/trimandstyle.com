b2f3c6a5722e9bb6c5827d9fa1f40bb47f2c3080
benjieedroso <ben09099@gmail.com>
2024-06-22 14:54:29 +0800

    add create profile view

---
+++ trimandstyle.com/Controllers/BarberController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using trimandstyle.com.Models;
+
+namespace trimandstyle.com.Controllers
+{
+    public class BarberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Store(Barber barber)
+        {
+
+            return barber.Name;
+        }
+        }
+    }
+
